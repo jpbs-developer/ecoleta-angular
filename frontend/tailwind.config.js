1b1b2445636aa485042429d01456615818aa9c4d
jpbs-developer <jpbs1@aluno.ifal.edu.br>
2024-05-11 21:13:17 -0300

    frontend: home

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: "#F0F0F5",
+        titles: "#322153",
+        texts: "#6C6C80",
+        green: "#34CB79",
+        "green-100": "#2FB86E",
+      },
+    },
   },
   plugins: [],
 };
